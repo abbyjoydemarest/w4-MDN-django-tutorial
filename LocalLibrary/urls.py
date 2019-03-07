38624c3a7ec39b1f8f72b5a90b27b20c8ede657f
Abby Demarest <abbyjoydemarest@gmail.com>
2019-03-07 18:47:50 -0500

    Finished part 8 section Setting Up Your Authentication Views

--- LocalLibrary/urls.py
+++ LocalLibrary/urls.py
@@ -25,7 +25,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),
     path('', RedirectView.as_view(url = '/catalog/', permanent = True)),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('accounts/', include('django.contrib.auth.urls')),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 #Use include() to add paths from the catalog application
 
