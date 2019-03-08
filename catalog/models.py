21fed536fee14db9eb603ec8125fca2456ca42ca
Abby Demarest <abbyjoydemarest@gmail.com>
2019-03-07 20:56:14 -0500

    almost done with 8, adjusted sidebar. cant get borrowed books to appear

--- catalog/models.py
+++ catalog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.urls import reverse  # Used to generate URLS by reversing the URL patterns
-import uuid #Required for unique book instances
+import uuid  #Required for unique book instances
+from django.contrib.auth.models import User
+from datetime import date
 
 # Create your models here.
 
@@ -65,6 +67,7 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
 
     LOAN_STATUS = (
@@ -82,7 +85,11 @@ class BookInstance(models.Model):
         default='m',
         help_text='Book availability',
     )
-
+    @property
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False 
 
     class Meta:
         ordering = ['due_back']
