e588c6bc4b2a5f1600c1a339a439b211eb8aa857
Abby Demarest <abbyjoydemarest@gmail.com>
2019-03-07 22:56:46 -0500

    finished part 8, did 1. of the challenge

--- catalog/views.py
+++ catalog/views.py
@@ -21,6 +21,7 @@ def index(request):
     num_visits = request.session.get('num_visits', 0)
     request.session['num_visits'] = num_visits + 1
 
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
